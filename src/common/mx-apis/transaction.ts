d9c018e457bdb3141704585013c7dee9ee06508c
Chris <nttriet9a4@gmail.com>
2023-08-18 20:01:48 +0700

    feat: update template

--- src/common/mx-apis/transaction.ts
+++ src/common/mx-apis/transaction.ts
@@ -1,7 +1,20 @@
 import { Config } from "../config";
+import axios from "axios";
+import axiosRetry from "axios-retry";
 
 let config = new Config("./config/config.yaml");
 
+// Configure retries for ETIMEDOUT error
+axiosRetry(axios, {
+  retries: 3, // Number of retries
+  retryCondition: (error: any) => {
+    // Retry on ETIMEDOUT error
+    return error.code && error.code === "ETIMEDOUT";
+  },
+  retryDelay: (retryCount: any) => {
+    return retryCount * 1000; // Time delay between retries in milliseconds
+  },
+});
 // Common Event Interface
 export interface Event {
   id: string;
@@ -14,21 +27,20 @@ export interface Event {
 }
 
 export async function txCount(address: string): Promise<number> {
-  const req = await fetch(
-    `${config.getApiUrl()}/accounts/${address}/transfers/count`,
-  );
-  return await req.json();
+  const req = `${config.getApiUrl()}/accounts/${address}/transfers/count`;
+  const response = await axios.get(req);
+  return response.data;
 }
 
 export async function TxHashes(
   address: string,
   from: number,
-  size: number,
+  size: number
 ): Promise<[string[], number]> {
   const req = `${config.getApiUrl()}/accounts/${address}/transfers?from=${from}&size=${size}`;
   console.log(req);
-  const txResponse = await fetch(req);
-  const jsonResponse = (await txResponse.json()) as any[];
+  const txResponse = await axios.get(req);
+  const jsonResponse = txResponse.data as any[];
   return [
     jsonResponse
       .map((tx: any) => {
@@ -49,13 +61,13 @@ export async function TxHashes(
 
 export async function getTransactionDetail(hash: string): Promise<any> {
   const req = `${config.getApiUrl()}/transactions/${hash}`;
-  const txResponse = await fetch(req);
-  return (await txResponse.json()) as any[];
+  const txResponse = await axios.get(req);
+  return txResponse.data as any[];
 }
 
 export async function filterEvent(
   trackingEvent: string[],
-  data: any,
+  data: any
 ): Promise<Event[] | undefined> {
   if (data.logs.events != undefined) {
     let events = data.logs.events;
